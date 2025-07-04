f313814c6ef99d902a1a470d9067788016d08d72
jrweqlop <herher.haha@windowslive.com>
2025-07-04 23:31:02 +0700

    update
    1.update axios

--- websetting/src/server/server.ts
+++ websetting/src/server/server.ts
@@ -5,5 +5,9 @@ const url = process.env.NEXT_PUBLIC_API || 'http://localhost:4500/api/v1'
 export const instance = axios.create({
     baseURL: url,
     timeout: 10000,
-    headers: { 'X-Custom-Header': 'foobar' }
+    headers: {
+        'X-Custom-Header': 'foobar', 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 });
